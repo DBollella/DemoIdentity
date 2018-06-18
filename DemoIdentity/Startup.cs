9818ad7617b06e2288a978574731996814796ffa
DBollella <douglas.bollella@gmail.com>
2018-06-17 22:09:19 -0300

    implementação de Microsoft.AspNetCore.Authentication.Facebook -version 1.1

--- DemoIdentity/Startup.cs
+++ DemoIdentity/Startup.cs
@@ -75,6 +75,12 @@ namespace DemoIdentity
 
             app.UseIdentity();
 
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
+
             // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
 
             app.UseMvc(routes =>
